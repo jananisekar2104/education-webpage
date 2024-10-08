2884a198b5a0cc2b317a6fa1ba15f5207edce002
jananisekar2104 <janusekar2104@gmail.com>
2024-10-09 00:49:56 +0530

    Add files via upload

---
+++ src/components/common/heading/Head.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Head = () => {
+  return (
+    
+    <div>
+        <section className='head'>
+            <div className="container flexSB">
+                <div className ="logo">
+                    <h1>SHIKSHAA SIMPLE LEARN</h1>
+                    <span>SOFTWARE TRANING CENTER</span>
+                </div>
+                <div className='social'>
+                    <i className='fab fa-facebook icon'></i>
+                    <i className='fab fa-instagram icon'></i>
+                    <i className="fab fa-twitter-square icon"></i>
+                    <i className="fab fa-youtube icon"></i>
+
+                </div>
+            </div>
+        </section>
+    </div>
+  
+  )
+}
+
+export default Head
---
+++ src/components/home/hero/Hero.jsx
@@ -0,0 +1,29 @@
+import React from "react"
+import Heading from "../../common/heading/Heading"
+import "./Hero.css"
+
+const Hero = () => {
+  return (
+    <>
+      <section className='hero'>
+        <div className='container'>
+          <div className='row'>
+            <Heading subtitle='WELCOME TO Shikshaa Simple Learn' title='Software Traing Center' />
+            <p>Training is our passion <b>Excellence is our goal</b>.</p>
+            <div className='button'>
+              <button className='primary-btn'>
+                GET STARTED NOW <i className='fa fa-long-arrow-alt-right'></i>
+              </button>
+              <button>
+                VIEW COURSE <i className='fa fa-long-arrow-alt-right'></i>
+              </button>
+            </div>
+          </div>
+        </div>
+      </section>
+      <div className='margin'></div>
+    </>
+  )
+}
+
+export default Hero
